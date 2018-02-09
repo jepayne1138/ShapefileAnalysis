a98c0e8d8738dc405630b76c51a1fbe4634fba60
James Payne <jpayne@steelking.onmicrosoft.com>
2018-02-09 12:40:41 -0600

    Fix misspelling and change console_script entry point to avoid conflict with module name

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     install_requires=requires,
     entry_points={
         'console_scripts': [
-            'analyize = analyize:main',
+            'analyzeshapefile = analyze:main',
         ],
     },
 )
